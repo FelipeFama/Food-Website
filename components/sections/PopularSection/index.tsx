334ace769ea65b78ed40770012ef05554aac10c0
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-29 14:39:21 -0300

    feat: add new component

---
+++ components/sections/PopularSection/index.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./style.module.scss";
+import { FaStar, FaRegStar } from "react-icons/fa";
+import popularBurgerImg from "../../../assets/popular/p-1.jpg";
+import popularCakeImg from "../../../assets/popular/p-2.jpg";
+import popularSweetsImg from "../../../assets/popular/p-3.jpg";
+import popularCupcakesImg from "../../../assets/popular/p-4.jpg";
+import popularDrinksImg from "../../../assets/popular/p-5.jpg";
+import popularIceCreamImg from "../../../assets/popular/p-6.jpg";
+
+const popularFoodImages = {
+  popularBurgerImage: {
+    source: popularBurgerImg,
+    alt: "burger",
+  },
+  popularCakeImage: {
+    source: popularCakeImg,
+    alt: "orange cake",
+  },
+  popularSweetImage: {
+    source: popularSweetsImg,
+    alt: "sweets",
+  },
+  popularCupcakesImage: {
+    source: popularCupcakesImg,
+    alt: "cupcake",
+  },
+  popularDrinksImage: {
+    source: popularDrinksImg,
+    alt: "drinks",
+  },
+  popularIceCreamImage: {
+    source: popularIceCreamImg,
+    alt: "ice-cream",
+  },
+};
+
+export default function PopularSection() {
+  return (
+    <section className={styles.popular} id="popular">
+      <h1 className={styles.heading}>
+        most<span>popular</span>foods
+      </h1>
+      <div className={styles.boxContainer}>
+        <div className={styles.box}>
+          <span className={styles.price}>$5 - $20</span>
+          <Image
+            src={popularFoodImages.popularBurgerImage.source}
+            alt={popularFoodImages.popularBurgerImage.alt}
+          />
+          <h3>tasty burger</h3>
+          <div className={styles.star}>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaRegStar></FaRegStar>
+          </div>
+          <a href="#" className={styles.btn}>
+            order now
+          </a>
+        </div>
+
+        <div className={styles.box}>
+          <span className={styles.price}>$5 - $20</span>
+          <Image
+            src={popularFoodImages.popularCakeImage.source}
+            alt={popularFoodImages.popularCakeImage.alt}
+          />
+          <h3>tasty cakes</h3>
+          <div className={styles.star}>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaRegStar></FaRegStar>
+          </div>
+          <a href="#" className={styles.btn}>
+            order now
+          </a>
+        </div>
+
+        <div className={styles.box}>
+          <span className={styles.price}>$5 - $20</span>
+          <Image
+            src={popularFoodImages.popularSweetImage.source}
+            alt={popularFoodImages.popularSweetImage.alt}
+          />
+          <h3>tasty sweets</h3>
+          <div className={styles.star}>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaRegStar></FaRegStar>
+          </div>
+          <a href="#" className={styles.btn}>
+            order now
+          </a>
+        </div>
+
+        <div className={styles.box}>
+          <span className={styles.price}>$5 - $20</span>
+          <Image
+            src={popularFoodImages.popularCupcakesImage.source}
+            alt={popularFoodImages.popularCupcakesImage.alt}
+          />
+          <h3>tasty cupcakes</h3>
+          <div className={styles.star}>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaRegStar></FaRegStar>
+          </div>
+          <a href="#" className={styles.btn}>
+            order now
+          </a>
+        </div>
+
+        <div className={styles.box}>
+          <span className={styles.price}>$5 - $20</span>
+          <Image
+            src={popularFoodImages.popularDrinksImage.source}
+            alt={popularFoodImages.popularDrinksImage.alt}
+          />
+          <h3>cold drinks</h3>
+          <div className={styles.stars}>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaRegStar></FaRegStar>
+          </div>
+          <a href="#" className={styles.btn}>
+            order now
+          </a>
+        </div>
+
+        <div className={styles.box}>
+          <span className={styles.price}>$5 - $20</span>
+          <Image
+            src={popularFoodImages.popularIceCreamImage.source}
+            alt={popularFoodImages.popularIceCreamImage.alt}
+          />
+          <h3>cold ice-cream</h3>
+          <div className={styles.star}>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaStar></FaStar>
+            <FaRegStar></FaRegStar>
+          </div>
+          <a href="#" className={styles.btn}>
+            order now
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+}
--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeSection from "./HomeSection";
 import SpecialitySection from "./SpecialitySection";
+import PopularSection from "./PopularSection";
 
 export default function Layout() {
   return (
     <>
       <HomeSection />
       <SpecialitySection />
+      <PopularSection />
     </>
   );
 }

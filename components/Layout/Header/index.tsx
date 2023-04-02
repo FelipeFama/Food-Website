1410cb3321dd769daba1d93dbb50afcab54b1f11
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-04-02 15:53:51 -0300

    feat: add menu responsive

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,20 +1,42 @@
-import { FaUtensils } from "react-icons/fa";
+import { useState } from "react";
+import { FaBars, FaTimes, FaUtensils } from "react-icons/fa";
 import styles from "./style.module.scss";
 
 export default function Header() {
+  const [navbarActive, setNavbarActive] = useState(false);
+
+  const toggleNavbar = () => setNavbarActive(!navbarActive);
+
   return (
     <header className={styles.header}>
       <a href="#" className={styles.logo}>
         <FaUtensils></FaUtensils>
         Food
       </a>
-      <nav className={styles.navbar}>
-        <a href="#home">home</a>
-        <a href="#speciality">speciality</a>
-        <a href="#popular">popular</a>
-        <a href="#gallery">gallery</a>
-        <a href="#review">review</a>
-        <a href="#order">order</a>
+
+      <div className={styles.menuBar} onClick={toggleNavbar}>
+        {navbarActive ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <nav className={`${styles.navbar} ${navbarActive ? styles.active : ""}`}>
+        <a href="#home" onClick={() => setNavbarActive(false)}>
+          home
+        </a>
+        <a href="#speciality" onClick={() => setNavbarActive(false)}>
+          speciality
+        </a>
+        <a href="#popular" onClick={() => setNavbarActive(false)}>
+          popular
+        </a>
+        <a href="#gallery" onClick={() => setNavbarActive(false)}>
+          gallery
+        </a>
+        <a href="#review" onClick={() => setNavbarActive(false)}>
+          review
+        </a>
+        <a href="#order" onClick={() => setNavbarActive(false)}>
+          order
+        </a>
       </nav>
     </header>
   );

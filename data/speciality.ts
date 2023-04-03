2e07e550bb9e6afc90edaeeb3a9d2170d84741d8
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-04-03 15:08:01 -0300

    feat: add data folder

---
+++ data/speciality.ts
@@ -0,0 +1,66 @@
+import burgerIconImg from "../assets/speciality/s-1.png";
+import pizzaIconImg from "../assets/speciality/s-2.png";
+import iceCreamIconImg from "../assets/speciality/s-3.png";
+import drinksIconImg from "../assets/speciality/s-4.png";
+import sweetIconImg from "../assets/speciality/s-5.png";
+import breakfastIconImg from "../assets/speciality/s-6.png";
+import burgerImg from "../assets/speciality/s-img-1.jpg";
+import pizzaImg from "../assets/speciality/s-img-2.jpg";
+import iceCreamImg from "../assets/speciality/s-img-3.jpg";
+import drinksImg from "../assets/speciality/s-img-4.jpg";
+import sweetImg from "../assets/speciality/s-img-5.jpg";
+import breakfastImg from "../assets/speciality/s-img-6.jpg";
+
+export const iconsFood = {
+  burgerIconImage: {
+    source: burgerIconImg,
+    alt: "burger icon",
+  },
+  pizzaIconImage: {
+    source: pizzaIconImg,
+    alt: "pizza icon",
+  },
+  iceCreamIconImage: {
+    source: iceCreamIconImg,
+    alt: "ice-cream icon",
+  },
+  drinksIconImage: {
+    source: drinksIconImg,
+    alt: "drinks icon",
+  },
+  sweetIconImage: {
+    source: sweetIconImg,
+    alt: "sweet icon",
+  },
+  breakfastIconImage: {
+    source: breakfastIconImg,
+    alt: "coffee icon",
+  },
+};
+
+export const specialityFoodImages = {
+  burgerImage: {
+    source: burgerImg,
+    alt: "burger",
+  },
+  pizzaImage: {
+    source: pizzaImg,
+    alt: "pizza",
+  },
+  iceCreamImage: {
+    source: iceCreamImg,
+    alt: "ice-cream",
+  },
+  drinksImage: {
+    source: drinksImg,
+    alt: "drinks",
+  },
+  sweetImage: {
+    source: sweetImg,
+    alt: "sweet",
+  },
+  breakfastImage: {
+    source: breakfastImg,
+    alt: "coffee",
+  },
+};

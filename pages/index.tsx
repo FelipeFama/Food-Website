35523dab816bd35c0d810ab30850eabb8fc83bfa
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-04-03 15:59:33 -0300

    Update icon project

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Food Restaurant Website</title>
         <meta name="description" content="Food Restaurant Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/foodicon.ico" />
       </Head>
       <Layout />
     </>

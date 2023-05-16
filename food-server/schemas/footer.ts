699a46ec0374df872b3a4fade002f38a00730172
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-05-16 18:51:05 -0300

    Update schemas

---
+++ food-server/schemas/footer.ts
@@ -0,0 +1,6 @@
+export default {
+  name: 'footer',
+  title: 'Footer',
+  type: 'document',
+  fields: [{name: 'span', title: 'Span', type: 'string'}],
+}
--- food-server/schemas/index.ts
+++ food-server/schemas/index.ts
@@ -1,3 +1,4 @@
+import header from './header'
 import home from './home'
 import speciality from './speciality'
 import popular from './popular'
@@ -5,5 +6,18 @@ import steps from './steps'
 import gallery from './gallery'
 import review from './review'
 import order from './order'
+import footer from './footer'
+import loader from './loader'
 
-export const schemaTypes = [home, speciality, popular, steps, gallery, review, order]
+export const schemaTypes = [
+  header,
+  home,
+  speciality,
+  popular,
+  steps,
+  gallery,
+  review,
+  order,
+  footer,
+  loader,
+]
